2f376b181cf9bd50864b60fdb3e4947c1eb5ff7e
csxstudios <csxstudios@gmail.com>
2017-03-08 15:11:08 -0500

    Add photos page title and button on Page 1

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { PhotosPage } from '../photos/photos';
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
-
+photosPage = PhotosPage;
   constructor(public navCtrl: NavController) {
     
   }
